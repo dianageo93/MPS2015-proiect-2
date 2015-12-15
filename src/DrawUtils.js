ac68c50acf52403e7dda4052a16f50bad380418f
Mihai Varga <mihai.mv13@gmail.com>
2015-12-15 09:52:25 +0200

    DrawUtils class

--- src/DrawUtils.js
+++ src/DrawUtils.js
@@ -1,1 +1,16 @@
 // Draw utils
+
+function State (angle, startCoords, specs) {
+    this.angle = angle;
+    this.startCoords = startCoords;
+}
+
+function DrawUtils () {
+    var canvas = document.getElementById('forest');
+    this.context = canvas.getContext('2d');
+    this.stateStack = [];
+    this.stateStack.push(new State(0, {x: canvas.clientWidth / 2, y: canvas.clientHeight}));
+}
+
+DrawUtils.prototype.draw(lSystem) {
+}
