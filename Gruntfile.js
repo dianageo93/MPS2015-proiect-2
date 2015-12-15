f0e3785445f4c205359a2dae373115d0cb4dba1e
Mihai Varga <mihai.mv13@gmail.com>
2015-12-15 08:47:24 +0200

    gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ';',
+            },
+            src: {
+                src: [
+                    'src/L-System.js',
+                    'src/DrawUtils.js'
+                ],
+                dest: 'dist/van-gogu.js'
+            }
+        },
+
+        mdlint: ['README.md']
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('build', ['concat']);
+};
