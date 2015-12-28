349490980901990798b596f2536ea1632a83ea57
Georgiana-Diana CIOCÎRDEL <georgiana.ciocirdel@stud.acs.upb.ro>
2015-12-28 20:36:30 +0200

    Adding two more valid tree options.

--- src/Seed.js
+++ src/Seed.js
@@ -9,3 +9,13 @@ var defaultOptions = {
     start: 'X',
     lineLength: 2
 };
+
+var options1 = JSON.parse(JSON.stringify(defaultOptions));
+options1.depth = 4;
+options1.rules = {'F': 'FF+[+F-F-F+F]-[-F+F-F]'};
+options1.start = 'F';
+
+var options2 = JSON.parse(JSON.stringify(defaultOptions));
+options2.depth = 7;
+options2.rules.X = 'F-[[X]+X]+F[+FX]-X'
+options2.lineLength = 25;
