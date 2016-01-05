da581346bf85061454823d40a5e7dcecabe1c528
Mihai Varga <mihai.mv13@gmail.com>
2016-01-05 15:07:46 +0200

    added a moon

--- src/Seed.js
+++ src/Seed.js
@@ -1,8 +1,14 @@
 var seed = [
     // mountain
-    {d: 4, r: {F: 'F-F+F+F-F'}, s: 'F', x: 1, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
-    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 5, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
-    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 10, y: 60, sa: -90, la: 90, ra: 90, ll: 9},
+    {d: 4, r: {F: 'F-F+F+F-F'}, s: 'F', x: 45, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
+    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 50, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
+    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 55, y: 60, sa: -90, la: 90, ra: 90, ll: 9},
+    // mountain
+    {d: 4, r: {F: 'F-F+F+F-F'}, s: 'F', x: 20, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
+    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 25, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
+    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 30, y: 60, sa: -90, la: 90, ra: 90, ll: 9},
+    // moon
+    {d: 11, r: {F: '+F--F+'}, s: 'F', x:90, y: 30, la: 45, ra: 45, ll: 2},
     // small bush
     {d: 4, r: {F: 'FF+[+F-F-F+F]-[-F+F-F]'}, s: 'F', x: 20},
     // large tree
