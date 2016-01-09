5332ca04077699d1af07e837d89047f65dd33ae2
Mihai Varga <mihai.mv13@gmail.com>
2016-01-09 12:44:37 +0200

    a better seed

--- src/Main.js
+++ src/Main.js
@@ -14,23 +14,37 @@ var defaultOptions = {
     color: '#000000'
 };
 
+function fillOptions(options) {
+    options.depth = options.d || defaultOptions.depth;
+    options.startAngle = options.sa || defaultOptions.startAngle;
+    options.leftAngle = options.la || defaultOptions.leftAngle;
+    options.rightAngle = options.ra || defaultOptions.rightAngle;
+    options.x = options.x || defaultOptions.x;
+    options.y = options.y || defaultOptions.y;
+    options.rules = options.r || defaultOptions.rules;
+    options.start = options.s || defaultOptions.start;
+    options.lineLength = options.ll || defaultOptions.lineLength;
+    options.color = options.c || defaultOptions.color;
+}
+
 window.addEventListener('load', function () {
     var drawUtils = new DrawUtils();
-    seed.forEach(function(options) {
-        options.depth = options.d || defaultOptions.depth;
-        options.startAngle = options.sa || defaultOptions.startAngle;
-        options.leftAngle = options.la || defaultOptions.leftAngle;
-        options.rightAngle = options.ra || defaultOptions.rightAngle;
-        options.x = options.x || defaultOptions.x;
-        options.y = options.y || defaultOptions.y;
-        options.rules = options.r || defaultOptions.rules;
-        options.start = options.s || defaultOptions.start;
-        options.lineLength = options.ll || defaultOptions.lineLength;
-        options.color = options.c || defaultOptions.color;
+    seeds.forEach(function(seed) {
+        var minX = seed.a[0];
+        var maxX = seed.a[1];
+        var minY = seed.a[2];
+        var maxY = seed.a[3];
 
-        var lsys = new LSystem(options);
-        var tree = lsys.generate();
+        for (var i = 0; i < seed.c; i++) {
+            var options = seed.lsys[Math.floor(Math.random() * seed.lsys.length)];
+            fillOptions(options);
+            options.x = minX + Math.floor((maxX - minX) * Math.random());
+            options.y = minY + Math.floor((maxY - minY) * Math.random());
 
-        drawUtils.draw(tree, options);
+            var lsys = new LSystem(options);
+            var tree = lsys.generate();
+
+            drawUtils.draw(tree, options);
+        }
     });
 });
--- src/Seed.js
+++ src/Seed.js
@@ -1,16 +1,37 @@
-var seed = [
-    // mountain
-    {d: 4, r: {F: 'F-F+F+F-F'}, s: 'F', x: 45, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
-    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 50, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
-    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 55, y: 60, sa: -90, la: 90, ra: 90, ll: 9},
-    // mountain
-    {d: 4, r: {F: 'F-F+F+F-F'}, s: 'F', x: 20, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
-    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 25, y: 60, sa: -90, la: 90, ra: 90, ll: 6},
-    {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', x: 30, y: 60, sa: -90, la: 90, ra: 90, ll: 9},
-    // moon
-    {d: 11, r: {F: '+F--F+'}, s: 'F', x:90, y: 30, la: 45, ra: 45, ll: 2},
-    // small bush
-    {d: 4, r: {F: 'FF+[+F-F-F+F]-[-F+F-F]'}, s: 'F', x: 20},
-    // large tree
-    {d: 7, r: {X: 'F-[[X]+X]+F[+FX]-X', F: 'FF'}, ll: 1, x: 40}
+var seeds = [
+{
+    lsys: [
+        {d: 4, r: {F: 'F-F+F+F-F'}, s: 'F', sa: -90, la: 90, ra: 90, ll: 6},
+        {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', sa: -90, la: 90, ra: 90, ll: 6},
+        {d: 3, r: {F: 'F-F+F+F-F'}, s: 'F', sa: -90, la: 90, ra: 90, ll: 9}
+    ],
+    a: [0, 80, 60, 60],
+    c: 8
+},
+{
+    lsys: [
+        {d: 11, r: {F: '+F--F+'}, s: 'F', la: 45, ra: 45, ll: 2}
+    ],
+    a: [90, 90, 20, 20],
+    c: 1
+},
+{
+    lsys: [
+        {d: 10, r: {X: 'X+YF+', Y: '-FX-Y'}, s:'FX', la: 90, ra: 90, ll: 5}
+    ],
+    a: [0, 100, 10, 20],
+    c: 2
+},
+{
+    lsys: [
+        {d: 4, r: {F: 'FF+[+F-F-F+F]-[-F+F-F]'}, s: 'F'},
+        {d: 7, r: {X: 'F-[[X]+X]+F[+FX]-X', F: 'FF'}, ll: 1},
+        {d: 5, r: {F: 'F[+F]F[-F][F]'}, s:'F', ll: 3, la: 20, ra: 20},
+        {d: 4, r: {F: 'FF-[-F+F+F]+[+F-F-F]'}, s:'F', ll: 5, la: 22.5, ra: 22.5},
+        {d: 7, r: {F: 'FF', X: 'F[+X]F[-X]+X'}, ll: 1, la: 20, ra: 20},
+        {d: 7, r: {F: 'FF', X: 'F[+X][-X]FX'}, ll: 1, la: 25.7, ra: 25.7},
+    ],
+    a: [0, 100, 80, 100],
+    c: 20
+}
 ];
